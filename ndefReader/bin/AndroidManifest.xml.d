11001b0fc78512de97039e504d6b4a20dd3fab76
Don Coleman <dcoleman@chariotsolutions.com>
2013-02-06 18:02:42 -0500

    ignore generated files

--- ndefReader/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/bin/AndroidManifest.xml : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/AndroidManifest.xml \
--- ndefReader/bin/NdefReader-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/bin/NdefReader-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/bin/NdefReader.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/libs/phonegap-nfc-android-0.4.1.jar \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/ndefReader/libs/cordova-2.3.0.jar \
