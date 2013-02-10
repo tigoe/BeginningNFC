e55117aae7d9970794c2e7101c20f98a66761743
Tom Igoe <tom.igoe@gmail.com>
2013-02-09 22:07:55 -0500

    Updated FoursquareCheckinAdvanced - not done

--- FoursquareCheckinAdvanced/assets/www/js/index.js
+++ FoursquareCheckinAdvanced/assets/www/js/index.js
@@ -1,108 +1,100 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
- /*
-
-    UNFINISHED!26 Jan 2013 -Tom Igoe
- */
 var app = {
-    // Application Constructor
+    // Application constructor
     initialize: function() {
         this.bindEvents();
+        console.log("Starting Foursquare Checkin app");
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
+    // bind any events that are required on startup to listeners:
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+
+    // this runs when the device is ready for user interaction:
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        var parentElement = document.getElementById("message");
+        parentElement.innerHTML = "Tap a tag to read its id number.";
+
         nfc.addTagDiscoveredListener(
-                app.onNfc,                      // tag successfully scanned
-                app.display("NFC initialized"), // NFC successfully initialized
-                app.display                     // Failed to initialize NFC
-            );
-    },
-     display: function(message) {
-            document.getElementById("message").innerHTML = message;
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+            app.onNfc,                                  // tag successfully scanned
+            function (status) {                         // listener successfully initialized
+                app.display("Listening for NFC tags.");
+            },
+            function (error) {                          // listener fails to initialize
+                app.display("NFC reader failed to initialize " + JSON.stringify(error));
+            }
+        )
     },
 
     onNfc: function(nfcEvent) {
         var tag = nfcEvent.tag;
-        app.display("Read tag: " + nfc.bytesToHexString(tag.id));
+        app.display("Read NDEF tag: " + nfc.bytesToHexString(tag.id));
     },
 
-    writeTag: function() {
-    	console.log('write tag');
-    	var tnf = parseInt(document.forms[0].elements["tnf"].value),
-    		recordType = document.forms[0].elements["recordType"].value,
-    		payload = nfc.stringToBytes(document.forms[0].elements["payload"].value),
-    		record;
-    	
-    		console.log("tnf: " + tnf + "  record type: " + recordType + "   payload: " + payload);
-    		
-    	switch (tnf) {
-	    	case ndef.TNF_WELL_KNOWN:
-	    		record = ndef.record(ndef.TNF_EXTERNAL_TYPE, nfc.stringToBytes(recordType), [], payload);
-	    		console.log("Well Known record.");
-	    		break;
-	    	case ndef.TNF_MIME_MEDIA:
-	    		record = ndef.mimeMediaRecord(recordType, payload);
-	    		console.log("MIME record.");
-	    		break;
-	    	case ndef.TNF_ABSOLUTE_URI:
-		    	payload.unshift(0x03);           // add URL shortener code
-	    	    record = Ndef.uriRecord(payload);
-	    	    console.log("URI record");
-	    		break;
-	    	case ndef.TNF_EXTERNAL_TYPE:
-	    	 	record = ndef.record(ndef.TNF_EXTERNAL_TYPE, nfc.stringToBytes(recordType), [], payload);
-	    		console.log("External record");
-	    		break;
-	    }
-	    
-	    console.log("record is type: " + tnf);
-	    	
-       // Put together the pieces for the NDEF record:
-       // var recordType = nfc.stringToBytes("android.com:pkg");				// record type: android application record
-       // var payload = nfc.stringToBytes("com.joelapenna.foursquared");		// application name
-       // create the actual NDEF record:
-       //  var record = ndef.record(ndef.TNF_EXTERNAL_TYPE, recordType, [], payload);	
+    display: function(message) {
+        document.getElementById("message").innerHTML = message;
+    },
 
+
+    formatMessage: function() {
+        var tnf = parseInt(document.forms[0].elements["tnf"].value),
+            recordType = document.forms[0].elements["recordType"].value,
+            payload = nfc.stringToBytes(document.forms[0].elements["payload"].value),
+            record;
+
+            console.log("tnf: " + tnf + "  record type: " + recordType + "   payload: " + payload);
+
+
+/*
+
+        I have work to do here. Make sure RTD is formatted correctly, make sure URL shorteners, etc
+        are attached properly. Probably make it less generic, for the purposes of teaching the lesson.
+        -Tom
+
+*/
+        switch (tnf) {
+            case ndef.TNF_WELL_KNOWN:
+                payload.unshift(0x03);           // add URL shortener code
+                record = ndef.record(ndef.TNF_WELL_KNOWN, nfc.stringToBytes(recordType), [], payload);
+                console.log("Well Known record.");
+                break;
+            case ndef.TNF_MIME_MEDIA:
+                payload.unshift(0x03);
+                record = ndef.mimeMediaRecord(recordType, payload);
+                console.log("MIME record.");
+                break;
+            case ndef.TNF_ABSOLUTE_URI:
+                payload.unshift(0x03);           // add URL shortener code
+                record = ndef.uriRecord(document.forms[0].elements["payload"].value);
+                console.log("URI record");
+                break;
+            case ndef.TNF_EXTERNAL_TYPE:
+                record = ndef.record(ndef.TNF_EXTERNAL_TYPE, nfc.stringToBytes(recordType), [], payload);
+                console.log("External record");
+                break;
+        }
+
+        console.log("record is type: " + tnf);
+
+       // Put together the pieces for the NDEF record:
+       // var recordType = nfc.stringToBytes("android.com:pkg");                // record type: android application record
+       // var payload = nfc.stringToBytes("com.joelapenna.foursquared");        // application name
+       // create the actual NDEF record:
+       //  var record = ndef.record(ndef.TNF_EXTERNAL_TYPE, recordType, [], payload);
+/*
+   // Put together the pieces for the NDEF record:
+        var recordType = nfc.stringToBytes("android.com:pkg");  // record type: android application record
+        var payload = nfc.stringToBytes("com.joelapenna.foursquared");  // application name
+
+        // create the actual NDEF record:
+        var record = ndef.record(ndef.TNF_EXTERNAL_TYPE, recordType, [], payload);
+*/
+        app.writeTag(record);
+
+
+    },
+
+    writeTag: function(record) {
         // write the record to the tag:
         nfc.write(
             [record],									// write the record itself to the tag
