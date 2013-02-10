e246f8794b7bfbb5d2ac31c2b6264db1b9c830dd
Don Coleman <dcoleman@chariotsolutions.com>
2013-02-10 16:53:24 -0500

    delete local.properties and generated directories

--- FoursquareCheckinAdvanced/bin/FoursquareCheckinAdvanced-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckinAdvanced/bin/FoursquareCheckinAdvanced-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckinAdvanced/bin/FoursquareCheckinAdvanced.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckinAdvanced/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckinAdvanced/libs/phonegap-nfc-android-0.4.1.jar \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckinAdvanced/libs/cordova-2.3.0.jar \
