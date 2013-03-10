5e6c3d7f3d43135f94db0dd5763ea7ad25bd6935
Tom Igoe <tom.igoe@gmail.com>
2013-03-10 10:51:03 -0400

    FoursquareCheckin cordova clean

--- FoursquareCheckin/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckin/bin/AndroidManifest.xml : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckin/AndroidManifest.xml \
--- FoursquareCheckin/bin/FoursquareCheckin-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckin/bin/FoursquareCheckin-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckin/bin/FoursquareCheckin.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckin/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/FoursquareCheckin/libs/cordova-2.3.0.jar \
