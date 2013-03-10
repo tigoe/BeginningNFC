5be3c578caa3a933c8fe6591f44e1f76e1e5f578
Tom Igoe <tom.igoe@gmail.com>
2013-03-10 11:08:15 -0400

    updated NdefReader to cordova 2.5.0

--- ndefReader/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/NdefReader/bin/AndroidManifest.xml : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/NdefReader/AndroidManifest.xml \
--- ndefReader/bin/NdefReader-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/NdefReader/bin/NdefReader-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/NdefReader/bin/NdefReader.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/NdefReader/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/NdefReader/libs/cordova-2.3.0.jar \
