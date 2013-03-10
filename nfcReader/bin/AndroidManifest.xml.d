6acd467beb5195d474a4a2bdfcaac55585489cd0
Tom Igoe <tom.igoe@gmail.com>
2013-03-10 10:45:28 -0400

    nfcReader cordova clean

--- nfcReader/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/nfcReader/bin/AndroidManifest.xml : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/nfcReader/AndroidManifest.xml \
--- nfcReader/bin/NfcReader-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/nfcReader/bin/NfcReader-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/nfcReader/bin/NfcReader.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/nfcReader/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/nfcReader/libs/cordova-2.3.0.jar \
