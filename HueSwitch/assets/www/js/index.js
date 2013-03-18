e625c8d38e9163db8fbd364fcbe977647941dc11
Don Coleman <dcoleman@chariotsolutions.com>
2013-03-17 20:11:42 -0400

    started impl

--- HueSwitch/assets/www/js/index.js
+++ HueSwitch/assets/www/js/index.js
@@ -1,49 +1,80 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
+    hueDeviceType: "NFC Switch",
+    hueUserName: "54658242a37981b1dd446c1656469853"
+    hueAddress: null,        
+    lightId: 1, // should be user configurable
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        findControllerAddress();
+        ensureAuthorized();
+        
+        nfc.addMimeTypeListener(
+            "text/hue",
+            app.onNfc, 
+            function() { console.log("listening for mime media tags"); },
+            function(error) { console.log("ERROR: " + JSON.stringify(error)); }
+            );
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onNfc: function(nfcEvent) {
+        var tag = nfcEvent.tag;
+    },
+    hue: function(settings) {
+        $.ajax({ 
+            type: 'PUT',
+            url: 'http://' + hueIP + '/api/' + hueAppId + '/lights/' + lightId + '/state',
+            data: JSON.stringify(settings),
+            success: function(data){
+                if (data[0].error) {
+                    alert(JSON.stringify(data));
+                }
+            },
+            error: function(xhr, type){
+                alert('Ajax error!')
+            }
+        });
+    },
+    findControllerAddress: function() {
+        
+        $.ajax({
+            url: 'http://www.meethue.com/api/nupnp',
+            dataType: 'json',
+            success: function(data) {
+                // expecting a list
+                if (data[0]) {
+                    app.hueIpAddress = data[0].internalipaddress;
+                    console.log(app.hueIpAddress);
+                }
+            },
+            error: function(xhr, type){
+                alert('Ajax error!')
+            }
+        });
+    },
+    ensureAuthorized: function() {
+        
+        var data = {"devicetype":"test user","username":"newdeveloper"};
+        
+        $.ajax({ 
+            type: 'POST',
+            url: 'http://' + hueIP + '/api',
+            data: JSON.stringify(settings),
+            success: function(data){
+                if (data[0].error) {
+                    // if not authorized, users gets an alert box
+                    alert(data[0].error.message);
+                }
+            },
+            error: function(xhr, type){
+                alert('Ajax error!')
+            }
+        });            
+        
     }
+    
 };
