a6331e398fc26918c0f55b2adcdfa9b13135226c
Tom Igoe <tom.igoe@gmail.com>
2013-03-21 22:26:57 -0400

    Cleaned up MimeWriter

--- MimeWriter/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/MimeWriter/bin/AndroidManifest.xml : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/MimeWriter/AndroidManifest.xml \
--- MimeWriter/bin/MimeWriter-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/MimeWriter/bin/MimeWriter-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/MimeWriter/bin/MimeWriter.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/MimeWriter/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/MimeWriter/libs/cordova-2.3.0.jar \
