be9649bcd779c5ba75662a35704094836bf76dee
Tom Igoe <tom.igoe@gmail.com>
2013-04-18 12:02:51 -0400

    deleted useless examples

--- CameraTagger/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/bin/AndroidManifest.xml : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/AndroidManifest.xml \
--- CameraTagger/bin/CameraTagger-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/bin/CameraTagger-debug-unaligned.apk : \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/bin/CameraTagger.ap_ \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/bin/classes.dex \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/libs/phonegap-nfc-android-0.4.3-SNAPSHOT.jar \
-/Users/tigoe/Documents/gitHub/GettingStartedWithNFC/CameraTagger/libs/cordova-2.3.0.jar \
