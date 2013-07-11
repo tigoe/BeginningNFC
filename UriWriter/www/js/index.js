7b591f86d15f501dae9a9cb07965844afd42c56b
tigoe <tom.igoe@gmail.com>
2013-07-11 07:02:13 -0400

    UriWriter reduced to 80 columns

--- UriWriter/www/js/index.js
+++ UriWriter/www/js/index.js
@@ -19,12 +19,13 @@ var app = {
         app.display("Tap a tag to see if it's compatible.");
 
         nfc.addNdefListener(
-            app.onNfc,                                  // tag successfully scanned
-            function (status) {                         // listener successfully initialized
+            app.onNfc,                      // tag successfully scanned
+            function (status) {             // listener successfully initialized
                 app.display("Listening for NDEF tags.");
             },
-            function (error) {                          // listener fails to initialize
-                app.display("NFC reader failed to initialize " + JSON.stringify(error));
+            function (error) {              // listener fails to initialize
+                app.display("NFC reader failed to initialize " +
+                	JSON.stringify(error));
             }
         );
     },
@@ -48,13 +49,14 @@ var app = {
     appends @message to the message div:
 */
     display: function(message) {
-        var display = document.getElementById("message"),   // the div you'll write to
-            label,                                          // what you'll write to the div
+    	// the div you'll write to:
+        var display = document.getElementById("message"),   
+            label,                                          // what you'll write
             lineBreak = document.createElement("br");       // a line break
 
         label = document.createTextNode(message);           // create the label
         display.appendChild(lineBreak);                     // add a line break
-        display.appendChild(label);                         // add the message node
+        display.appendChild(label);                         // add the message
     },
 /*
     clears the message div:
@@ -70,11 +72,11 @@ var app = {
     makeMessage: function() {
         // Put together the pieces for the NDEF message:
         // NDEF Type Name Format:
-        var tnf = parseInt(document.forms[0].elements["tnf"].value),
-            recordType = document.forms[0].elements["mimeType"].value, // RTD
-            payload = document.forms[0].elements["payload"].value,     // content
-            record,                         // NDEF record object
-            message = [];                   // NDEF Message to pass to writeTag()
+        var tnf = parseInt(tnf.value),
+            recordType = mimeType.value, // RTD
+            payload = payload.value,     // content
+            record,                      // NDEF record object
+            message = [];                // NDEF Message to pass to writeTag()
 
         // if the TNF is Well-Known, then write a URI record:
         if (tnf === ndef.TNF_WELL_KNOWN) {
@@ -96,14 +98,15 @@ var app = {
     writeTag: function(message) {
         // write the record to the tag:
         nfc.write(
-            message,						// write the record itself to the tag
-            function () {					// when complete, run this callback function:
-                app.clear();                            // clear the message div
-                app.display("Wrote data to tag.");		// notify the user in message div
-                navigator.notification.vibrate(100);	// vibrate the device as well
+            message,                      // write the record itself to the tag
+            function () {                 // when complete, run this callback
+                app.clear();                          // clear the message div
+                app.display("Wrote data to tag.");    // notify the user
+                navigator.notification.vibrate(100);  // vibrate the device
             },
-            function (reason) {				// this function runs if the write command fails
-                navigator.notification.alert(reason, function() {}, "There was a problem");
+            function (reason) {         // failure function
+                navigator.notification.alert(reason, function() {}, 
+                	"There was a problem");
             }
         );
     }
