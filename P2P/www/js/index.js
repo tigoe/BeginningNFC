f0d8d6b4a9f7fdbeadea788e355dc736af293401
tigoe <tom.igoe@gmail.com>
2013-07-11 20:19:25 -0400

    reduced display and clear in P2P

--- P2P/www/js/index.js
+++ P2P/www/js/index.js
@@ -258,20 +258,16 @@ var app = {
     /*
         appends @message to the message div:
     */
-    display: function(message) {
-        var display = document.getElementById("message"),   // the div you'll write to
-            label,                                          // what you'll write to the div
-            lineBreak = document.createElement("br");       // a line break
-
-        label = document.createTextNode(message);           // create the label
-        display.appendChild(lineBreak);                     // add a line break
-        display.appendChild(label);                         // add the message node
+    display: function(thisText) {
+        var label = document.createTextNode(thisText),
+        	lineBreak = document.createElement("br");
+        message.appendChild(lineBreak);					// add a line break
+        message.appendChild(label);						// add the text
     },
     /*
         clears the message div:
     */
     clear: function() {
-        var display = document.getElementById("message");
-        display.innerHTML = "";
+         message.innerHTML = "";
     }
 };      // end of app
