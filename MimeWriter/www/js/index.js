8b37c02533578bac10d226a14c001283b456e7b8
tigoe <tom.igoe@gmail.com>
2013-07-12 10:06:59 -0400

    Changed MimeWrite 4 to 3

--- MimeWriter/www/js/index.js
+++ MimeWriter/www/js/index.js
@@ -1,127 +1,127 @@
 var app = {
 /*
-    Application constructor
+   Application constructor
 */
-    initialize: function() {
-        this.bindEvents();
-        console.log("Starting MIME Message Writer app");
-    },
+   initialize: function() {
+      this.bindEvents();
+      console.log("Starting MIME Message Writer app");
+   },
 
-    // bind any events that are required on startup to listeners:
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+   // bind any events that are required on startup to listeners:
+   bindEvents: function() {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+   },
 /*
-    this runs when the device is ready for user interaction:
+   this runs when the device is ready for user interaction:
 */
-    onDeviceReady: function() {
-        var parentElement = document.getElementById("message");
-        parentElement.innerHTML = "Tap a tag to see if it's compatible.";
+   onDeviceReady: function() {
+      var parentElement = document.getElementById("message");
+      parentElement.innerHTML = "Tap a tag to see if it's compatible.";
 
-        nfc.addNdefListener(
-            app.onNfc,                                  // tag successfully scanned
-            function (status) {                         // listener successfully initialized
-                app.display("Listening for NDEF tags.");
-            },
-            function (error) {                          // listener fails to initialize
-                app.display("NFC reader failed to initialize " + JSON.stringify(error));
-            }
-        )
+      nfc.addNdefListener(
+         app.onNfc,                          // tag successfully scanned
+         function (status) {                 // listener successfully initialized
+            app.display("Listening for NDEF tags.");
+         },
+         function (error) {                  // listener fails to initialize
+            app.display("NFC reader failed to initialize " + JSON.stringify(error));
+         }
+      )
 
-         nfc.addNdefFormatableListener(
-            app.onFormat,                                  // tag successfully scanned
-            function (status) {                         // listener successfully initialized
-                app.display("Listening for NDEF-Formattable tags.");
-            },
-            function (error) {                          // listener fails to initialize
-                app.display("NFC reader failed to initialize " + JSON.stringify(error));
-            }
-        )
+       nfc.addNdefFormatableListener(
+         app.onFormat,                        // tag successfully scanned
+         function (status) {                  // listener successfully initialized
+            app.display("Listening for NDEF-Formattable tags.");
+         },
+         function (error) {                   // listener fails to initialize
+            app.display("NFC reader failed to initialize " + JSON.stringify(error));
+         }
+      )
 
-    },
-
-/*
-    runs when a blank formattable tag shows up.
-    displays tag ID from @nfcEvent in message div:
-*/
-    onFormat: function(nfcEvent) {
-        var tag = nfcEvent.tag;         // get the tag
-        app.clear();                    // clear the message div
-
-        // write a message to the message div:
-        app.display("Read NDEF tag: " + nfc.bytesToHexString(tag.id));
-        app.display("This tag can be written to.");
-    },
+   },
 
 /*
-    runs when an NDEF-formatted tag shows up.
-    displays tag ID from @nfcEvent in message div:
+   runs when a blank formattable tag shows up.
+   displays tag ID from @nfcEvent in message div:
 */
-    onNfc: function(nfcEvent) {
-        var tag = nfcEvent.tag;         // get the tag
-        app.clear();                    // clear the message div
+   onFormat: function(nfcEvent) {
+      var tag = nfcEvent.tag;    // get the tag
+      app.clear();               // clear the message div
 
-        // write a message to the message div:
-        app.display("Read NDEF tag: " + nfc.bytesToHexString(tag.id));
-        app.display("This tag can be written to.");
-        app.display("WARNING: you'll over-write the content on it already.");
-     },
-
+      // write a message to the message div:
+      app.display("Read NDEF tag: " + nfc.bytesToHexString(tag.id));
+      app.display("This tag can be written to.");
+   },
 
 /*
-    appends @message to the message div:
+   runs when an NDEF-formatted tag shows up.
+   displays tag ID from @nfcEvent in message div:
 */
-    display: function(message) {
-        var display = document.getElementById("message"),   // the div you'll write to
-            label,                                          // what you'll write to the div
-            lineBreak = document.createElement("br");       // a line break
+   onNfc: function(nfcEvent) {
+      var tag = nfcEvent.tag;    // get the tag
+      app.clear();               // clear the message div
 
-        label = document.createTextNode(message);           // create the label
-        display.appendChild(lineBreak);                     // add a line break
-        display.appendChild(label);                         // add the message node
-    },
-/*
-    clears the message div:
-*/
-    clear: function() {
-        var display = document.getElementById("message");
-        display.innerHTML = "";
+      // write a message to the message div:
+      app.display("Read NDEF tag: " + nfc.bytesToHexString(tag.id));
+      app.display("This tag can be written to.");
+      app.display("WARNING: you'll over-write the content on it already.");
     },
 
-/*
-    makes an NDEF message and calls writeTag() to write it to a tag:
-*/
-    makeMessage: function() {
-        // Put together the pieces for the NDEF message:
-        var tnf = ndef.TNF_MIME_MEDIA,               // NDEF Type Name Format
-            recordType = document.forms[0].elements["mimeType"].value, // RTD
-            payload = document.forms[0].elements["payload"].value,     // content
-            record,                         // NDEF record object
-            message = [];                   // NDEF Message to pass to writeTag()
-
-        // create the actual NDEF record:
-        record = ndef.record(tnf, recordType, [], payload);
-        // put the record in the message array:
-        message.push(record);
-        //write the message:
-        app.writeTag(message);
-    },
 
 /*
-    writes NDEF message @message to a tag:
+   appends @message to the message div:
 */
-    writeTag: function(message) {
-        // write the record to the tag:
-        nfc.write(
-            message,						// write the record itself to the tag
-            function () {					// when complete, run this callback function:
-                app.clear();                            // clear the message div
-                app.display("Wrote data to tag.");		// notify the user in message div
-                navigator.notification.vibrate(100);	// vibrate the device as well
-            },
-            function (reason) {				// this function runs if the write command fails
-                navigator.notification.alert(reason, function() {}, "There was a problem");
-            }
-        );
-    }
-};          // end of app
+   display: function(message) {
+      var display = document.getElementById("message"), // the div you'll write to
+         label,                                         // what you'll write to the div
+         lineBreak = document.createElement("br");      // a line break
+
+      label = document.createTextNode(message);         // create the label
+      display.appendChild(lineBreak);                   // add a line break
+      display.appendChild(label);                       // add the message node
+   },
+/*
+   clears the message div:
+*/
+   clear: function() {
+      var display = document.getElementById("message");
+      display.innerHTML = "";
+   },
+
+/*
+   makes an NDEF message and calls writeTag() to write it to a tag:
+*/
+   makeMessage: function() {
+      // Put together the pieces for the NDEF message:
+      var tnf = ndef.TNF_MIME_MEDIA,            // NDEF Type Name Format
+         recordType = document.forms[0].elements["mimeType"].value, // RTD
+         payload = document.forms[0].elements["payload"].value,     // content
+         record,                   // NDEF record object
+         message = [];             // NDEF Message to pass to writeTag()
+
+      // create the actual NDEF record:
+      record = ndef.record(tnf, recordType, [], payload);
+      // put the record in the message array:
+      message.push(record);
+      //write the message:
+      app.writeTag(message);
+   },
+
+/*
+   writes NDEF message @message to a tag:
+*/
+   writeTag: function(message) {
+      // write the record to the tag:
+      nfc.write(
+         message,                 // write the record itself to the tag
+         function () {            // when complete, run this callback function:
+            app.clear();                     // clear the message div
+            app.display("Wrote data to tag.");     // notify the user in message div
+            navigator.notification.vibrate(100);   // vibrate the device as well
+         },
+         function (reason) {     // this function runs if the write command fails
+            navigator.notification.alert(reason, function() {}, "There was a problem");
+         }
+      );
+   }
+};        // end of app
