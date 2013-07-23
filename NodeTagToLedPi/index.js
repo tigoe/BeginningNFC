a2bd8b1f7933bf67d187c8a9f48b8f2d523e838b
tigoe <tom.igoe@gmail.com>
2013-07-22 23:37:43 -0400

    Added NodeTagToLedPi

---
+++ NodeTagToLedPi/index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node     // let the shell know you're a node script
+
+var ndef = require('ndef'),               // require ndef package
+    mifare = require('mifare-classic'),   // require this package
+	 io = require('onoff').Gpio, 				// require onoff package
+	 led = [];										// array of LEDs
+	 
+
+/*
+led[0] = new Gpio(18, 'out'),					// set LED I/O pins as outputs	
+led[1] = new Gpio(23, 'out'),
+led[2] = new Gpio(24, 'out'),
+led[4] = new Gpio(25, 'out'),
+*/
+
+led[0] = new Gpio(gpio1_21, 'out'),					// set LED I/O pins as outputs	
+led[1] = new Gpio(gpio1_21, 'out'),
+led[2] = new Gpio(gpio1_21, 'out'),
+led[4] = new Gpio(gpio1_21, 'out'),
+  
+setInterval(readTags, 2000);           	// read every 2 seconds
+
+function readTags() { 
+   mifare.read(function(error, buffer) {  // read tag
+      if (error) {                        // if there's an error result
+        console.log("Read failed ");      // let user know about the error
+        console.log(error);
+      } else {                            // you got an NDEF message
+         // decode the message into a JSON object:
+         var message = ndef.decodeMessage(buffer.toJSON());
+         // print the message's records:
+         console.log("Found NDEF message with " + message.length +
+            // "record" if there's only one, "records" if there's more:
+            (message.length === 1 ? " record" : " records" ));
+         // print the message:
+         console.log(ndef.stringify(message));  
+         // loop over the LEDs and turn on one for each record: 
+         for (var pinNum=0; pinNum<4; pinNum++) {            
+            if (pinNum < message.length) {   // LED0 = 1 record, LED1 = 2 records, etc
+               io.digitalWrite(pin, 1);   	
+               led[pinNum].writeSync(1);		// turn on pin
+            } else {
+               io.digitalWrite(pin, 0);   	
+               led[pinNum].writeSync(0);		// turn off pin
+            }
+         }  
+      }
+   });
+}
+
