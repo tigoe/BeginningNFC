a5417fb4641b1e79d2f11e42955431130806ab39
Don Coleman <dcoleman@chariotsolutions.com>
2013-08-28 12:07:18 -0400

    write nfc every 3 seconds, and some cleanup

--- HotelTagWriter/HotelTagWriter.ino
+++ HotelTagWriter/HotelTagWriter.ino
@@ -9,40 +9,36 @@
  */
 
 // SEEED STUDIO
-#include <SPI.h>
-#include <PN532SPI.h>
+//#include <SPI.h>
+//#include <PN532SPI.h>
 // end SEEED STUDIO
 
 // ADAFRUIT
-//#include <Wire.h>
-//#include <PN532_I2C.h>
+#include <Wire.h>
+#include <PN532_I2C.h>
 // end ADAFRUIT
 
 #include <PN532.h>
 #include <NfcAdapter.h>
 
 // SEEED STUDIO
-PN532SPI pn532spi(SPI, 10);
-NfcAdapter nfc = NfcAdapter(pn532spi);
+//PN532SPI pn532spi(SPI, 10);
+//NfcAdapter nfc = NfcAdapter(pn532spi);
 // end SEEED STUDIO
 
 // ADAFRUIT
-//PN532_I2C pn532_i2c(Wire);
-//NfcAdapter nfc = NfcAdapter(pn532_i2c);
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
 // end ADAFRUIT
 
-//#include <Wire.h>
-//#include <Adafruit_NFCShield_I2C.h>
-//#include <NfcAdapter.h>
-
 const int greenLed = 9;         // pin for the green LED
-const int redLed = 8;          // pin for the red LED
+const int redLed = 8;           // pin for the red LED
 
-//NfcAdapter nfc = NfcAdapter();  // instance of the NFC adapter library
 String inputString = "";        // string for input from serial port
 long lightOnTime = 0;           // last time the LEDs were turned on, in ms
+long lastReadTime = 0;          // last time we checked for NFC tag, in ms
 
-boolean readyToWrite = false;
+boolean readyToWrite = false;   // true when we are ready to write to NFC tag
 
 void setup() {
   Serial.begin(9600);           // initialize serial communications
@@ -54,14 +50,21 @@ void setup() {
 void loop() {
   
   while (Serial.available() > 0) {
-    //processChar(Serial.read());
+
     char thisChar = Serial.read();
     // add incoming character to the end of inputString:
     inputString += thisChar; 
-    // if the incoming character is }, you're done. Look for a tag:
-    if (thisChar == '}') {
+
+    if (thisChar == '{') {
+      // new message, reset buffer
+      inputString = "{";
+      readyToWrite = false;
+    } else if (thisChar == '}') {
+      // end of message, ready to write to tag
+      Serial.println("Ready to write data to tag");
       readyToWrite = true;
     }
+
   }
   
   if (readyToWrite) {
@@ -76,25 +79,24 @@ void loop() {
 
 void lookForTag() {
 
-  if (nfc.tagPresent()) {                       // if there's a tag present
-    NdefMessage message;                        // make a new NDEF message
-    message.addTextRecord(inputString);         // add the input string as a record
-    boolean success = nfc.write(message);       // attempt to write to the tag
+  if (millis() - lastReadTime > 3000) {           // read every three seconds
+    if (nfc.tagPresent()) {                       // if there's a tag present
+      NdefMessage message;                        // make a new NDEF message
+      message.addTextRecord(inputString);         // add the input string as a record
+      boolean success = nfc.write(message);       // attempt to write to the tag
     
-    if (success) {
-      Serial.println("Result: tag written."); // let the desktop app know you succeeded
-      inputString = "";                       // clear the string for another read
-      digitalWrite(greenLed, HIGH);           // turn on the success light
-      lightOnTime = millis();
-      readyToWrite = false; // TODO should allow keep writing, reset input on "{"
-    } else {
-      // let the desktop app know you failed:
-      Serial.println("Result: failed to write to tag"); 
-      digitalWrite(redLed, HIGH);              // turn on the failure light
-      lightOnTime = millis();
+      if (success) {
+        Serial.println("Result: tag written.");   // let the desktop app know you succeeded
+        digitalWrite(greenLed, HIGH);             // turn on the success light
+        lightOnTime = millis();
+      } else {
+        // let the desktop app know you failed:
+        Serial.println("Result: failed to write to tag"); 
+        digitalWrite(redLed, HIGH);              // turn on the failure light
+        lightOnTime = millis();
+      }
     }
+    lastReadTime = millis();
   } 
 }
-
-
 
