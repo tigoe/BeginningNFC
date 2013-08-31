decd2c5fa097f783e3475eae3b4530a484c850b3
Don Coleman <dcoleman@chariotsolutions.com>
2013-08-30 23:37:32 -0400

    check TNF and type inline getPayloadAsString (uses less memory) other cleanup

--- HotelTagReader/HotelTagReader.ino
+++ HotelTagReader/HotelTagReader.ino
@@ -7,35 +7,27 @@
  checkout: checkout time (unix time, long int),
  }
  */
- 
-// This code requires the new Seeed PN532 Drivers
-// https://github.com/xiongyihui/PN532.git
-// There is one git repo, but it must be included as 3 libraries. I use symlinks.
-// This also requries the modified version of Ndef
-// https://github.com/xiongyihui/Ndef.git
 
 // Uncomment the SEEED or ADAFRUIT sections below based on which shield you are using
 
 // SEEED STUDIO
 //#include <SPI.h>
 //#include <PN532SPI.h>
+//#include <PN532.h>
+//#include <NfcAdapter.h>
+//#include <Time.h>
+//
+//PN532_SPI pn532spi(SPI, 10);
+//NfcAdapter nfc = NfcAdapter(pn532spi);
 // end SEEED STUDIO
 
 // ADAFRUIT
 #include <Wire.h>
 #include <PN532_I2C.h>
-// end ADAFRUIT
-
 #include <PN532.h>
 #include <NfcAdapter.h>
 #include <Time.h>
 
-// SEEED STUDIO
-//PN532SPI pn532spi(SPI, 10);
-//NfcAdapter nfc = NfcAdapter(pn532spi);
-// end SEEED STUDIO
-
-// ADAFRUIT
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 // end ADAFRUIT
@@ -56,17 +48,17 @@ void setup() {
   Serial.begin(9600);
   // set the clock to the date & time
   // hour (24-hour format), minute, second, day, month, year:
-  setTime(00, 00, 00, 29, 8, 2013); 
-  
+  setTime(00, 00, 00, 1, 9, 2013);
+
   nfc.begin();                   // initialize the NFC reader
   pinMode(lockPin, OUTPUT);      // make the door lock pin an output
   digitalWrite(lockPin, LOW);    // set it low to lock the door
   pinMode(greenLed, OUTPUT);     // make pin 9 an output
   pinMode(redLed, OUTPUT);       // make pin 10 an output
 
-  Serial.println("\nHotel NDEF Reader");  
-  Serial.println("Current Hotel Time is " + formatTime(now()));
-  Serial.print("This is the lock for room ");Serial.println(roomNumber);
+  Serial.println(F("\nHotel NDEF Reader"));
+  Serial.print(F("Current Hotel Time is "));Serial.println(formatTime(now()));
+  Serial.print(F("This is the lock for room "));Serial.println(roomNumber);
 }
 
 void loop() {
@@ -74,7 +66,7 @@ void loop() {
     digitalWrite(greenLed, goodRead);    // green LED lights if you get a good read
     digitalWrite(lockPin, goodRead);     // lock opens if you get a good read
     digitalWrite(redLed, !goodRead);     // red LED lights if you don't
-  } 
+  }
   else {                                // after three seconds, lock no matter what
     digitalWrite(greenLed, LOW);        // turn off green LED
     digitalWrite(redLed, LOW);          // turn off red LED
@@ -92,71 +84,65 @@ void resetValues() {
 }
 
 boolean listenForTag() {
-  
+
   boolean unlockDoor = false;
   resetValues();
 
   if (nfc.tagPresent()) {         // if there's a tag present
-    readTime = millis();          // timestamp the last time you saw a card  
+    readTime = millis();          // timestamp the last time you saw a card
     NfcTag tag = nfc.read();
-    if (tag.hasNdefMessage()) {   // every tag won't have a message        
+    if (tag.hasNdefMessage()) {   // every tag won't have a message
       NdefMessage message = tag.getNdefMessage();
       NdefRecord record = message.getRecord(0);
 
-      // TODO we should be checking the TNF and Type
-      
-      // Assume the payload can be converted to a String
-      String payload = getPayloadAsString(record);
-      parsePayload(payload);   
+      if (record.getTnf() == TNF_MIME_MEDIA && record.getType() == "application/json") {
 
-      // check if you can let them in or not:
-      unlockDoor = isValidKey(); 
-    } 
-  } 
+        // payload is a byte array
+        int payloadLength = record.getPayloadLength();
+        byte payload[payloadLength];
+        record.getPayload(payload);
+
+        // convert the payload to a String
+        String json = "";
+        for (int c=0; c< payloadLength; c++) {
+          json += (char)payload[c];
+        }
+
+        parsePayload(json);
+
+        // check if you can let them in or not:
+        unlockDoor = isValidKey();
+      }
+    }
+  }
   return unlockDoor;
 }
 
 /*
-  Convert the NdefRecord payload from a byte array into a String
-*/
-String getPayloadAsString(NdefRecord record) {
-  int payloadLength = record.getPayloadLength();
-  byte payload[payloadLength];
-  record.getPayload(payload);
-      
-  // this block would make a nice helper function, to return the payload as a String:
-  String result = "";
-  for (int c=0; c < payloadLength; c++) {
-    result += (char)payload[c]; 
-  }
-  return result;
-}
-
-/* 
   Parse the JSON data from the payload String
   Save the values we care about into local variables
 */
 void parsePayload(String data) {
   // you only care about what's between the brackets, so:
-  int openingBracket = data.indexOf('{');      
+  int openingBracket = data.indexOf('{');
   int closingBracket = data.indexOf('}');
   // your individual data is between two commas:
   int lastComma = openingBracket;
   int comma = 0;
   // parse the data until the last comma:
   while (comma != -1){
-    String key, value;      
+    String key, value;
     int colon = data.indexOf(':', lastComma); // get the next colon
     comma = data.indexOf(',', colon);         // get the next comma
-
     // key is between the last comma and the colon:
-    key = data.substring(lastComma+1, colon); 
+    key = data.substring(lastComma+1, colon);
+
     // if there are no more commas:
     if (comma == -1) {    // value is between colon and closing:
       value = data.substring(colon+1, closingBracket);
-    } 
+    }
     else {                // value is between colon and next comma:
-      value = data.substring(colon+1, comma); 
+      value = data.substring(colon+1, comma);
     }
 
     // now to get rid of the quotation marks:
@@ -168,75 +154,72 @@ void parsePayload(String data) {
     // now, look for the possible data you care about:
     setValue(key, value);
     lastComma = comma;
-  } 
+  }
 }
 
 /*
   Set the variable if it's something we care about, ignore other values.
  */
 void setValue(String thisKey, String thisValue) {
-  
-  if (thisKey == "checkout"){ 
+
+  if (thisKey == "checkout"){
     checkout = thisValue.toInt();
-  } else if (thisKey == "checkin") {       
+  } else if (thisKey == "checkin") {
     checkin = thisValue.toInt();
   } else if (thisKey == "name") {
-    cardName = thisValue; 
+    cardName = thisValue;
   } else if (thisKey == "room") {
     cardRoomNumber = thisValue.toInt();
-  } 
-  
+  }
+
 }
 
 /*
-  Check to see if the current time is between the checkin time
- and the checkout time. Return true if so:
+  Check to see if this key should open this door.
+  Does the room number match? Is the current time between
+  the checkin time and checkout time?
  */
 boolean isValidKey() {
   boolean result = false;
-  time_t arrival = checkin;
-  time_t departure = checkout;
-  
+
   if (cardRoomNumber == roomNumber) {
     if (now() <= checkin) {
       Serial.println("You haven't checked in yet.");
       Serial.println("Current time " + formatTime(now()));
       Serial.println("Your arrival " + formatTime(checkin));
-      
+
     } else if ((now() >= checkin) && (now() <= checkout))  {
       Serial.println("Welcome back to your room, " + cardName + ".");
-      result = true; 
-      
+      result = true;
+
     } else if (now() >= checkout) {
       Serial.println("Thanks for staying with us! You've checked out.");
       Serial.println("Current time " + formatTime(now()));
       Serial.println("Your departure " + formatTime(checkout));
-      
-    } 
+
+    }
   } else {
     Serial.print("This card can't unlock room ");
     Serial.print(roomNumber);
     Serial.println(".");
-    
+
   }
   return result;
 }
 
 String formatTime(time_t time) {
-  TimeElements element; // TODO rename me
-  breakTime(time, element);
+  TimeElements elements;
+  breakTime(time, elements);
   String formatted = "";
-  formatted += element.Month;
+  formatted += elements.Month;
   formatted += "/";
-  formatted += element.Day;
+  formatted += elements.Day;
   formatted += "/";
-  formatted += element.Year + 1970;
+  formatted += elements.Year + 1970;
   formatted += " ";
-  formatted += element.Hour;
+  formatted += elements.Hour;
   formatted += ":";
-  formatted += element.Minute;
+  formatted += elements.Minute;
   return formatted;
 }
-
-
 
