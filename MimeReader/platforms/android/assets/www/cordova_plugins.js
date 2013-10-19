bb2ef26ab34347530075b73344fb3730aee79fe5
Don Coleman <dcoleman@chariotsolutions.com>
2013-10-18 22:51:11 -0400

    remove android

--- MimeReader/platforms/android/assets/www/cordova_plugins.js
+++
@@ -1,9 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
-        "id": "com.chariotsolutions.nfc.plugin.NFC",
-        "runs": true
-    }
-]
-});
